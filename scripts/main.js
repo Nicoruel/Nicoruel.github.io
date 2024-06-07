57eec066ced26c91b2b60b8b27e21e51c44af3c6
Nicoruel <144718530+Nicoruel@users.noreply.github.com>
2024-06-07 19:26:27 +0000

    Final Website unless i fnd a problem

--- scripts/main.js
+++ scripts/main.js
@@ -1,59 +1,20 @@
-console.log("Task 1");
-for (i=0; i<11; i++){ console.log(i*i)}
+function ChangeSpinnerDirection() {
+       let spinDiv = document.querySelector(".logospin");
+       if (spinDiv.style.animationDirection == "") {
 
-console.log("Task 2");
-for (let i=5;i>=1; i--)
-{console.log(i)}
-console.log("Blastoff!");
+           spinDiv.style.animationDirection = "reverse";
+       }
+       else {
+           spinDiv.style.animationDirection = "";
+       }
+   } 
+function ChangeSpinnerSpeed() {
+       let spinDiv = document.querySelector(".logospin");
+       if (spinDiv.style.animationDuration == "") {
 
-console.log("Task 3");
-for (let i=0; i<=50; i+=2)
-    console.log(i)
-
-console.log("Task 4");
-let sum = 0;
-for (let i = 1; i <= 100; i++) {
-    sum += i;
-}
-console.log(sum);
-console.log("Task 5");
-let factorial = 1;
-for (let i = 1; i <= 10; i++){
-    factorial *= i;
-    console.log(factorial);
-}
-console.log("Task 6");
-let arr = [3, 1, 4, 1, 5];
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i])
-}
-console.log("Task 7");
-for (let i = 0; i < arr.length; i++){
-  console.log(arr.at(-i-1));
-}
-console.log("Task 8");
-let cubeNumbers = [];
-for (let i = 1; i <=10; i++){
-  cubeNumbers.push(i**3);
-}
-console.log(cubeNumbers);
-console.log("Task 9");
-let fibonacci = [];
-let first = 0;
-let second = 1;
-for(let i =1; i <11; i++) {
-
-fibonacci .push (first)
-let temp = second
-second = second + first;
-first = temp
-}
-
-console.log(fibonacci);
-console.log("Task 10");
-let reversedArr=[];
-for(let i = arr.length-1; i>=0;i--){
-  reversedArr.push(arr[i]);
-}
-console.log(reversedArr);
-    
+           spinDiv.style.animationDuration = "750ms";
+       }
+       else {
+           spinDiv.style.animationDuration = "";
+       }
+   }
